c6e269c97c8909aa529c9b6b4e55dca2067cf3be
Maksim <max3691@yandex.ru>
2020-02-14 13:52:55 -0500

    demo_done

--- UnityMultiplayerSample-master/Assets/Scripts/GameScript.cs
+++ UnityMultiplayerSample-master/Assets/Scripts/GameScript.cs
@@ -18,7 +18,12 @@ public class GameScript : MonoBehaviour
     [SerializeField] Transform player;
     [SerializeField] Transform ai;
  
-    [SerializeField] GameObject text;
+    [SerializeField] GameObject playerWinTxt;
+    [SerializeField] GameObject aiWinTxt;
+    [SerializeField] GameObject playerLoseTxt;
+    [SerializeField] GameObject aiLoseTxt;
+    [SerializeField] GameObject playerDrawTxt;
+    [SerializeField] GameObject aiDrawTxt;
 
     int playerChoose = 0;
     int aiChoose = 0;
@@ -30,7 +35,6 @@ public class GameScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (playersTurn && playerChoose <= 0)
             return;
 
@@ -41,47 +45,23 @@ public class GameScript : MonoBehaviour
         }
     }
 
-    public void ProcessPlayerSelection(int choose)
-    {
-        playerChoose = choose;
-        playersTurn = false;
-        if (playerChoose == 1)
-        {
-            var choice = Instantiate(rock, player);
-            Destroy(choice, 2.0f);
-        }
-
-        else if (playerChoose == 2)
-        {
-            var choice = Instantiate(scissors, player);
-            Destroy(choice, 2.0f);
-        }
-
-        else if (playerChoose == 3)
-        {
-            var choice = Instantiate(paper, player);
-            Destroy(choice, 2.0f);
-        }
-
-    }
-
     void GenerateAISelection()
     {
         int random = UnityEngine.Random.Range(1, 4);
 
         aiChoose = random;
 
-        if (aiChoose == 1)
+        if (aiChoose == (int)States.Rock)
         {
              var choice = Instantiate(rock, ai);
              Destroy(choice, 2.0f);
         }
-        else if (aiChoose == 2)
+        else if (aiChoose == (int)States.Scissors)
         {
             var choice = Instantiate(scissors, ai);
             Destroy(choice, 2.0f);
         }
-        else if (aiChoose == 3)
+        else if (aiChoose == (int)States.Paper)
         {
             var choice = Instantiate(paper, ai);
             Destroy(choice, 2.0f);
@@ -92,35 +72,84 @@ public class GameScript : MonoBehaviour
 
     void ChooseWinner()
     {
+        //Deactivates all the win/lost texts
+        aiDrawTxt.SetActive(false);
+        playerDrawTxt.SetActive(false);
+        aiLoseTxt.SetActive(false);
+        aiWinTxt.SetActive(false);
+        playerLoseTxt.SetActive(false);
+        playerWinTxt.SetActive(false);
+
+
+        //Activates win/lose text based on win/lose condition
         if (playerChoose == aiChoose)
         {
             //draw
+            aiDrawTxt.SetActive(true);
+            playerDrawTxt.SetActive(true);
         }
         else if (playerChoose == (int)States.Paper && aiChoose == (int)States.Rock)
         {
             //player won
+            aiLoseTxt.SetActive(true);
+            playerWinTxt.SetActive(true);
         }
         else if (playerChoose == (int)States.Paper && aiChoose == (int)States.Scissors)
         {
             //ai won
+            aiWinTxt.SetActive(true);
+            playerLoseTxt.SetActive(true);
         }
         else if (playerChoose == (int)States.Rock && aiChoose == (int)States.Scissors)
         {
             //player won
+            aiLoseTxt.SetActive(true);
+            playerWinTxt.SetActive(true);
         }
         else if (playerChoose == (int)States.Rock && aiChoose == (int)States.Paper)
         {
             //ai won
+            aiWinTxt.SetActive(true);
+            playerLoseTxt.SetActive(true);
         }
         else if (playerChoose == (int)States.Scissors && aiChoose == (int)States.Rock)
         {
             //ai won
+            aiWinTxt.SetActive(true);
+            playerLoseTxt.SetActive(true);
         }
         else if (playerChoose == (int)States.Scissors && aiChoose == (int)States.Paper)
         {
             //player won
+            aiLoseTxt.SetActive(true);
+            playerWinTxt.SetActive(true);
         }
         playerChoose = 0;
         playersTurn = true;
+    }
+
+    //Calls every time one of the buttons is pressed
+    public void ProcessPlayerSelection(int choose)
+    {
+        playerChoose = choose;
+        playersTurn = false;
+        if (playerChoose == (int)States.Rock)
+        {
+            var choice = Instantiate(rock, player);
+            Destroy(choice, 2.0f);
+        }
+
+        else if (playerChoose == (int)States.Scissors)
+        {
+            var choice = Instantiate(scissors, player);
+            Destroy(choice, 2.0f);
+        }
+
+        else if (playerChoose == (int)States.Paper)
+        {
+            var choice = Instantiate(paper, player);
+            Destroy(choice, 2.0f);
+        }
+
     }
 }
