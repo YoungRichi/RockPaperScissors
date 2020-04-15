5b469e2b0d19e01eb533576e2e7041dfb7dd5423
Maksim <max3691@yandex.ru>
2020-04-15 12:32:09 -0400

    activeGame class added

---
+++ UnityMultiplayerSample-master/Assets/Scripts/NetworkScripts/ActiveGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Networking.Transport;
+
+public class ActiveGame
+{
+    private UdpNetworkDriver m_driver;
+    private NetworkConnection m_con1;
+    private NetworkConnection m_con2;
+
+    public ActiveGame(UdpNetworkDriver driver, NetworkConnection con1, NetworkConnection con2)
+    {
+        m_driver = driver;
+        m_con1 = con1;
+        m_con2 = con2;
+    }
+
+    // Process
+    public void GameProcess() {
+
+    }
+    // IsActive    
+}
